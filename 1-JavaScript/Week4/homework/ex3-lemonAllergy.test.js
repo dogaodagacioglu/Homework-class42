d6b1415e85919a7cb73c7aa245d4671963bf8c5e
Doğa ödağacıoğlu <agarwoodson@icloud.com>
2023-01-16 16:46:12 +0200

    I commit before creating new branch

--- 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
@@ -26,26 +26,29 @@ const fruitBasket = [
 ];
 
 // ! Function under test
-function sanitizeFruitBasket(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function sanitizeFruitBasket(generic, allergy) {
+  const freshBasket = generic.filter ((fruit)=> fruit !== allergy);
+  return freshBasket
 }
 
 // ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(sanitizeFruitBasket).toHaveLength(2);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(originalFruitBasketContents).toEqual(fruitBasket);
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    const actual = sanitizeFruitBasket(fruitBasket, 'lemon');
+    const expected = ['apple','grapefruit','banana','watermelon'];
+    expect(actual).toEqual(expected);
   });
 });
