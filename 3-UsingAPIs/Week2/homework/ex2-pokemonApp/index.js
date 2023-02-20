6d7a529401210e905c6810fe123370b94219d20d
Doğa ödağacıoğlu <agarwoodson@icloud.com>
2023-02-20 23:55:37 +0200

    homework complete

--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 'use strict';
 /*------------------------------------------------------------------------------
 Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-UsingAPIs/Week2/README.md#exercise-2-gotta-catch-em-all
@@ -23,7 +24,6 @@ Try and avoid using global variables. As much as possible, try and use function
 parameters and return values to pass data back and forth.
 ------------------------------------------------------------------------------*/
 
-const url = 'https://pokeapi.co/api/v2/pokemon/'
 
 function fetchData(url) {
   return fetch(url)
@@ -43,35 +43,55 @@ function fetchData(url) {
   })
 }
 
-async function fetchAndPopulatePokemons(elements) {
-  const data = await fetchData(url);
-  data.result.forEach(pokemon => {
-    const table = document.createElement('select'); 
-    table.textContent = pokemon.name
-    elements.append(table);
-  })
+  function fetchAndPopulatePokemons(elements) {
   
-}
-
-async function fetchImage(url) {
-  const data = await fetchData(url);
-  data.result.forEach(img => {
-    const imgPokemon = document.createElement('img');
-    imgPokemon.src = img.url;
-    
+  const{results} = elements
+  const select = document.createElement('select');
+  select.classList='select-box'
+  
+   
+  results.forEach(pokemon => {
+    const option = document.createElement('option');
+    option.classList = 'option-box';
+    option.textContent = pokemon.name
+    select.appendChild(option)
+    document.getElementById('pokemon-container').appendChild(select);
+    option.style.background = '#716F71';
   })
   
 
+  document.getElementById('get-pokemon').addEventListener('click',openButton);
+  function openButton(){
+    select.style.width = '18vh';
+  }
+
+
+  select.onchange = function () {
+    const selectedOption = this.options[this.selectedIndex];
+    console.log(selectedOption);
+    const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selectedOption.index +1}.png`;
+    fetchImage(imgUrl);
+  };
+
 }
+
+async function fetchImage(imgUrl) {
+  const imgPokemon = document.createElement('img');
+  imgPokemon.src= imgUrl;
+
+  const container =document.getElementById('img-container');
+  container.innerHTML = '';
+  container.appendChild(imgPokemon);
+}
+
+
+
 
 async function main() {
   try {
     await fetchData('https://pokeapi.co/api/v2/pokemon?limit=151')
-      .then(data=>{
+      .then(data=>{ 
         fetchAndPopulatePokemons(data)
-      })
-      .then(data=>{
-        fetchImage(data)
       })
   } catch(error){
     console.log(error);
